49dd5f4d4def99b5d761ba8e6ee3ae2f21e9bdf0
laurenrcwest <140652895+laurenrcwest@users.noreply.github.com>
2023-07-27 20:15:43 -0700

    Update streamlit_app.py
    add question about fruit you'd like information about

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,10 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
 
