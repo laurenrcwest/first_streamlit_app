682041e988f446cef6dec15ca8ae94e4aac17a4d
laurenrcwest <140652895+laurenrcwest@users.noreply.github.com>
2023-07-27 19:51:13 -0700

    Update streamlit_app.py
    add back in non-normalized json

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,12 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.text(fruityvice_response.json())
+
 # normalized the json data
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # displays normalized json data
 streamlit.text(fruityvice_normalized)
 
