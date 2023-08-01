d25d2e866d77ed33d05c393650654a2cc97f735f
laurenrcwest <140652895+laurenrcwest@users.noreply.github.com>
2023-08-01 16:42:04 -0700

    Update streamlit_app.py
    use full database name

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ streamlit.text("The fruit load list contains:")
 #snowflake functions
 def get_fruit_load_list():
       with my_cnx.cursor() as my_cur:
-            my_cur.execute("select * from fruit_load_list")
+            my_cur.execute("select * from PC_RIVERY_DB.PUBLICfruit_load_list")
             return my_cur.fetchall()
 #button to load fruit
 if streamlit.button('Get Fruit Load List'):
