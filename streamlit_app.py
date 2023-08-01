a4b7de95c6597d554be29c0c3650b43a4c124884
laurenrcwest <140652895+laurenrcwest@users.noreply.github.com>
2023-08-01 14:52:43 -0700

    Update streamlit_app.py
    add indentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,24 +32,20 @@ streamlit.write('The user entered ', fruit_choice)
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-  streamlit.error ("Please select a fruit to get information about it.")
-else:
-
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+  fruit_choice)
-
-# normalized the json data
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# displays normalized json data
-streamlit.dataframe(fruityvice_normalized)
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error ("Please select a fruit to get information about it.")
+  else:
+    streamlit.write('The user entered ', fruit_choice)
+    #import requests
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+  fruit_choice)
+    # normalized the json data
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # displays normalized json data
+     streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
-streamlit.error()
+    streamlit.error()
 
 #stop code from here for troubleshooting
 streamlit.stop()
