09b5c57691d970619255392b7db06431c3966a0b
laurenrcwest <140652895+laurenrcwest@users.noreply.github.com>
2023-08-01 15:30:28 -0700

    Update streamlit_app.py
    correct typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ try:
       if not fruit_choice:
             streamlit.error ("Please select a fruit to get information about it.")
       else:
-            back_from_fuction=_fruityvice_data(this_fruit_choice)
+            back_from_fuction=get_fruityvice_data(this_fruit_choice)
             streamlit.dataframe(back_from_function)
 
 
