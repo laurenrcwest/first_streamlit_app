404ad273ecd86541e30ae24325e41544d2b8eaaf
laurenrcwest <140652895+laurenrcwest@users.noreply.github.com>
2023-08-01 13:29:09 -0700

    Update streamlit_app.py
    re-enter to fetch all rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 streamlit.text("The fruit load list contains:")
 my_cur.execute("SELECT * from fruit_load_list ")
-my_data_rows = my_cur.fetchall()
+my_data_rows = my_cur.fetch.all()
 streamlit.text(my_data_rows)
 
 
