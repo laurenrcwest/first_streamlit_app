fd9d276f49206046032f1026ac62eb7effe0ed0c
laurenrcwest <140652895+laurenrcwest@users.noreply.github.com>
2023-08-01 17:38:11 -0700

    Update streamlit_app.py
    remove old header

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ try:
 except URLError as e:
       streamlit.error()
 
-streamlit.text("The fruit load list contains:")
+
 #snowflake functions
 def get_fruit_load_list():
       with my_cnx.cursor() as my_cur:
